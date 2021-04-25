cde3c3cf0729ba64d04ed8593d9911ea2d104f94
Fung Yeung <yeung.kei.fung@gmail.com>
2021-04-25 15:57:10 +0800

    completed ex.1

--- exercise/spiders/ex1.py
+++ exercise/spiders/ex1.py
@@ -3,8 +3,14 @@ import scrapy
 
 class Ex1Spider(scrapy.Spider):
     name = 'ex1'
-    allowed_domains = ['https://scrapingclub.com/exercise/detail_basic/']
-    start_urls = ['http://https://scrapingclub.com/exercise/detail_basic//']
+    allowed_domains = ['scrapingclub.com']
+    start_urls = ['https://scrapingclub.com/exercise/detail_basic']
 
     def parse(self, response):
-        pass
+        path = "//div[contains(@class, 'card-body')]/{}/text()"
+        yield dict(
+            title = response.xpath(path.format('h3')).get(),
+            price = response.xpath(path.format('h4')).get(),
+            desc = response.xpath(path.format('p')).get()
+        )
+
